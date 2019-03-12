3fc0eef148b49e9bfd11b1f14b119d681205188d
dehunter456 <dehunter456@gmail.com>
2019-03-12 18:06:06 -0500

    cleanup

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 5000
-
-app.get('/api_call', (req, res) => res.send({message: 'Hello World!'}))
-
-app.listen(port, () => console.log(`Listening on port ${port}.`))
